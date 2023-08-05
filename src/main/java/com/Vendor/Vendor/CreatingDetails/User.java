e18d40a6d8aa5345c698aad7d6df940fb55c138d
Bharath P R <bharathkumarpr1999@gmail.com>
2023-08-05 21:23:43 +0530

    first

---
+++ src/main/java/com/Vendor/Vendor/CreatingDetails/User.java
@@ -0,0 +1,35 @@
+package com.Vendor.Vendor.CreatingDetails;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity()
+@Data
+@Validated
+@Table(name = "Vendor_user")
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private long phoneNumber;
+    private String last_name;
+    private String log_in;
+    private String password;
+    private int cart;
+    private String buy;
+    private int quantity;
+    @OneToMany
+    private List<Product> product;
+
+}
